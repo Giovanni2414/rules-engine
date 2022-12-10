d86e0abb4bce6395b989401788c41041e7ede59d
nicolaspenagos <nicolas.penagosm98@gmail.com>
2022-12-10 10:10:46 -0500

    feat(settingUp):setting up the Vue project and adding expression minimal components

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Column from '../components/expression/Column.vue'
+import Card from '../components/Card.vue'
+</script>
+
+<template>
+  <main>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+
+    <Column>
+    </Column>
+    <Card>
+
+    </Card>
+
+  </main>
+</template>
